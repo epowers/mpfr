d0cd0727e5e7188a15b5c5e28ce5e57e6a7fbdec
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2004-02-20 01:04:10 +0000

    Ported bug fix / test of gmp_op.c (0.5+3/2 freezes) to the 2.0 branch.
    git-svn-id: svn://scm.gforge.inria.fr/svn/mpfr/branches/mpfr-2-0-2-branch@2771 280ebfd0-de03-0410-8827-d642c229c3f4

--- mpfr-test.h
+++ mpfr-test.h
@@ -1,6 +1,6 @@
 /* auxiliary functions for MPFR tests.
 
-Copyright 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+Copyright 1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
 
 This file is part of the MPFR Library.
 
@@ -68,3 +68,5 @@ double Ulp _PROTO ((double));
 int Isnan _PROTO ((double));
 void d_trace _PROTO ((const char *, double));
 void ld_trace _PROTO ((const char *, long double));
+
+#define mpfr_set_str1(x,s) mpfr_set_str(x,s,10,GMP_RNDN)
