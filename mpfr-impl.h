79599da80b79fc35400f49a8615bf66a36b398fa
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2005-09-02 14:32:14 +0000

    Avoid MIPSpro / IRIX64 (incorrect) optimizations for DOUBLE_ISNAN. [2.2 branch]
    git-svn-id: svn://scm.gforge.inria.fr/svn/mpfr/branches/mpfr-2-2-branch@3778 280ebfd0-de03-0410-8827-d642c229c3f4

--- mpfr-impl.h
+++ mpfr-impl.h
@@ -345,7 +345,13 @@ typedef union ieee_double_extract Ieee_double_extract;
                          (((Ieee_double_extract *)&(x))->s.manh != 0)))
 #else
 # define DOUBLE_ISINF(x) ((x) > DBL_MAX || (x) < -DBL_MAX)
-# define DOUBLE_ISNAN(x) ((x) != (x))
+# if MPFR_NANISNAN
+/* Avoid MIPSpro / IRIX64 (incorrect) optimizations.
+   The + must not be replaced by a ||. */
+#  define DOUBLE_ISNAN(x) (!(((x) >= 0.0) + ((x) <= 0.0)))
+# else
+#  define DOUBLE_ISNAN(x) ((x) != (x))
+# endif
 #endif
 
 
