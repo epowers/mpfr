1e8655d4267d63f7d32f15e75a28182588154e8c
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2005-09-06 15:02:12 +0000

    Patches from the trunk: added mpfr_lngamma, fixed mpfr_gamma, more detailed error message in tversion. [2.2 branch]
    git-svn-id: svn://scm.gforge.inria.fr/svn/mpfr/branches/mpfr-2-2-branch@3794 280ebfd0-de03-0410-8827-d642c229c3f4

--- tests/tgamma.c
+++ tests/tgamma.c
@@ -24,9 +24,8 @@ MA 02110-1301, USA. */
 
 #include "mpfr-test.h"
 
-int mpfr_gamma (mpfr_ptr, mpfr_srcptr, mp_rnd_t);
-
 #define TEST_FUNCTION mpfr_gamma
+#define RAND_FUNCTION(x) mpfr_random2(x, MPFR_LIMB_SIZE (x), 1)
 #include "tgeneric.c"
 
 static void
@@ -189,6 +188,7 @@ static void
 special_overflow (void)
 {
   mpfr_t x, y;
+  mp_exp_t emin = mpfr_get_emin ();
 
   set_emin (-125);
   set_emax (128);
@@ -199,8 +199,159 @@ special_overflow (void)
   mpfr_gamma (y, x, GMP_RNDN);
   if (!mpfr_inf_p (y))
     {
-      printf("Overflow error.\n");
+      printf ("Overflow error.\n");
       mpfr_dump (y);
+      exit (1);
+    }
+
+  /* problem mentioned by Kenneth Wilder, 18 Aug 2005 */
+  mpfr_set_prec (x, 29);
+  mpfr_set_prec (y, 29);
+  mpfr_set_d (x, -200000000.5, GMP_RNDN); /* exact */
+  mpfr_gamma (y, x, GMP_RNDN);
+  if (!(mpfr_zero_p (y) && MPFR_SIGN (y) < 0))
+    {
+      printf ("Error for gamma(-200000000.5)\n");
+      printf ("expected -0");
+      printf ("got      ");
+      mpfr_dump (y);
+      exit (1);
+    }
+
+  mpfr_set_prec (x, 53);
+  mpfr_set_prec (y, 53);
+  mpfr_set_str (x, "-200000000.1", 10, GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  if (!(mpfr_zero_p (y) && MPFR_SIGN (y) < 0))
+    {
+      printf ("Error for gamma(-200000000.1), prec=53\n");
+      printf ("expected -0");
+      printf ("got      ");
+      mpfr_dump (y);
+      exit (1);
+    }
+
+  /* another problem mentioned by Kenneth Wilder, 29 Aug 2005 */
+  mpfr_set_prec (x, 333);
+  mpfr_set_prec (y, 14);
+  mpfr_set_str (x, "-2.0000000000000000000000005", 10, GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 14);
+  mpfr_set_str_binary (x, "-11010011110001E66");
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma(-2.0000000000000000000000005)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  /* another tests from Kenneth Wilder, 31 Aug 2005 */
+  set_emax (200);
+  set_emin (-200);
+  mpfr_set_prec (x, 38);
+  mpfr_set_prec (y, 54);
+  mpfr_set_str_binary (x, "0.11101111011100111101001001010110101001E-166");
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 54);
+  mpfr_set_str_binary (x, "0.100010001101100001110110001010111111010000100101011E167");
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma (test 1)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  set_emax (1000);
+  set_emin (-2000);
+  mpfr_set_prec (x, 38);
+  mpfr_set_prec (y, 71);
+  mpfr_set_str_binary (x, "10101011011100001111111000010111110010E-1034");
+  /* 184083777010*2^(-1034) */
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 71);
+  mpfr_set_str_binary (x, "10111111001000011110010001000000000000110011110000000011101011111111100E926");
+  /* 1762885132679550982140*2^926 */
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma (test 2)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  mpfr_set_prec (x, 38);
+  mpfr_set_prec (y, 88);
+  mpfr_set_str_binary (x, "10111100111001010000100001100100100101E-104");
+  /* 202824096037*2^(-104) */
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 88);
+  mpfr_set_str_binary (x, "1010110101111000111010111100010110101010100110111000001011000111000011101100001101110010E-21");
+  /* 209715199999500283894743922*2^(-21) */
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma (test 3)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  mpfr_set_prec (x, 171);
+  mpfr_set_prec (y, 38);
+  mpfr_set_str (x, "-2993155353253689176481146537402947624254601559176535", 10,
+                GMP_RNDN);
+  mpfr_div_2exp (x, x, 170, GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 38);
+  mpfr_set_str (x, "201948391737", 10, GMP_RNDN);
+  mpfr_mul_2exp (x, x, 92, GMP_RNDN);
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma (test 5)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  set_emin (-500000);
+  mpfr_set_prec (x, 337);
+  mpfr_set_prec (y, 38);
+  mpfr_set_str (x, "-30000.000000000000000000000000000000000000000000001", 10,
+                GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  mpfr_set_prec (x, 38);
+  mpfr_set_str (x, "-3.623795987425E-121243", 10, GMP_RNDN);
+  if (mpfr_cmp (x, y))
+    {
+      printf ("Error for gamma (test 7)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  /* was producing infinite loop */
+  set_emin (emin);
+  mpfr_set_prec (x, 71);
+  mpfr_set_prec (y, 71);
+  mpfr_set_str (x, "-200000000.1", 10, GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  if (!(mpfr_zero_p (y) && MPFR_SIGN (y) < 0))
+    {
+      printf ("Error for gamma (test 8)\n");
+      printf ("expected "); mpfr_dump (x);
+      printf ("got      "); mpfr_dump (y);
+      exit (1);
+    }
+
+  set_emax (1073741823);
+  mpfr_set_prec (x, 29);
+  mpfr_set_prec (y, 29);
+  mpfr_set_str (x, "423786866", 10, GMP_RNDN);
+  mpfr_gamma (y, x, GMP_RNDN);
+  if (!mpfr_inf_p (y) || mpfr_sgn (y) < 0)
+    {
+      printf ("Error for gamma(423786866)\n");
       exit (1);
     }
 
@@ -213,6 +364,7 @@ special_overflow (void)
 int
 main (void)
 {
+  MPFR_TEST_USE_RANDS ();
   tests_start_mpfr ();
 
   special ();
