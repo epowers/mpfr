epowers/mpfr

commit b70e087b548cc505fdff58c3b35ddca7778127e5

Fixed bug reported by Chris Saunders: if _GMP_IEEE_FLOATS is defined, the build of tset_ld.c fails because DOUBLE_ISNAN expects a lvalue. * mpfr-impl.h: documented that for such macros, the argument must   be a lvalue, and always make sure that it is a lvalue (so that   a failure doesn't depend on the environment). * tests/tset_ld.c: fixed the bug.
git-svn-id: svn://scm.gforge.inria.fr/svn/mpfr/trunk@5880 280ebfd0-de03-0410-8827-d642c229c3f4

mpfr-impl.h
@@ -395,7 +395,7 @@ static double double_zero = 0.0;
 # define DBL_NEG_ZERO (-0.0)
 #endif
 
-/* for x of type ieee_double_extract */
+/* Note: the argument x must be a lvalue of type double. */
 #if _GMP_IEEE_FLOATS
 typedef union ieee_double_extract Ieee_double_extract;
 
@@ -407,16 +407,18 @@ typedef union ieee_double_extract Ieee_double_extract;
                          ((((Ieee_double_extract *)&(x))->s.manl != 0) || \
                          (((Ieee_double_extract *)&(x))->s.manh != 0)))
 #else
-# define DOUBLE_ISINF(x) ((x) > DBL_MAX || (x) < -DBL_MAX)
+/* Below, the &(x) == &(x) allows to make sure that x is a lvalue
+   without (probably) any warning from the compiler. */
+# define DOUBLE_ISINF(x) (&(x) == &(x) && ((x) > DBL_MAX || (x) < -DBL_MAX))
 # ifdef MPFR_NANISNAN
 /* Avoid MIPSpro / IRIX64 / gcc -ffast-math (incorrect) optimizations.
    The + must not be replaced by a ||. With gcc -ffast-math, NaN is
    regarded as a positive number or something like that; the second
    test catches this case. */
 #  define DOUBLE_ISNAN(x) \
-    (!((((x) >= 0.0) + ((x) <= 0.0)) && -(x)*(x) <= 0.0))
+    (&(x) == &(x) && !((((x) >= 0.0) + ((x) <= 0.0)) && -(x)*(x) <= 0.0))
 # else
-#  define DOUBLE_ISNAN(x) ((x) != (x))
+#  define DOUBLE_ISNAN(x) (&(x) == &(x) && (x) != (x))
 # endif
 #endif
 

tests/tset_ld.c
@@ -48,7 +48,8 @@ check_gcc33_bug (void)
 static int
 Isnan_ld (long double d)
 {
-  if (DOUBLE_ISNAN ((double) d))
+  double e = (double) d;
+  if (DOUBLE_ISNAN (e))
     return 1;
   LONGDOUBLE_NAN_ACTION (d, goto yes);
   return 0;

commit 0e115b190cf1af29e5418f8ecd5dca68b219d355

mpfr-impl.h: avoid a compiler bug under Mac OS X Tiger + Xcode (&x == &x is false) that makes tset_d fail; this problem was introduced in r5880. [merged -c 5883 from trunk]
git-svn-id: svn://scm.gforge.inria.fr/svn/mpfr/branches/2.4@5884 280ebfd0-de03-0410-8827-d642c229c3f4

mpfr-impl.h
@@ -407,18 +407,21 @@ typedef union ieee_double_extract Ieee_double_extract;
                          ((((Ieee_double_extract *)&(x))->s.manl != 0) || \
                          (((Ieee_double_extract *)&(x))->s.manh != 0)))
 #else
-/* Below, the &(x) == &(x) allows to make sure that x is a lvalue
-   without (probably) any warning from the compiler. */
-# define DOUBLE_ISINF(x) (&(x) == &(x) && ((x) > DBL_MAX || (x) < -DBL_MAX))
+/* Below, the &(x) == &(x) || &(x) != &(x) allows to make sure that x
+   is a lvalue without (probably) any warning from the compiler.  The
+   &(x) != &(x) is needed to avoid a failure under Mac OS X 10.4.11
+   (with Xcode 2.4.1, i.e. the latest one). */
+# define LVALUE(x) (&(x) == &(x) || &(x) != &(x))
+# define DOUBLE_ISINF(x) (LVALUE(x) && ((x) > DBL_MAX || (x) < -DBL_MAX))
 # ifdef MPFR_NANISNAN
 /* Avoid MIPSpro / IRIX64 / gcc -ffast-math (incorrect) optimizations.
    The + must not be replaced by a ||. With gcc -ffast-math, NaN is
    regarded as a positive number or something like that; the second
    test catches this case. */
 #  define DOUBLE_ISNAN(x) \
-    (&(x) == &(x) && !((((x) >= 0.0) + ((x) <= 0.0)) && -(x)*(x) <= 0.0))
+    (LVALUE(x) && !((((x) >= 0.0) + ((x) <= 0.0)) && -(x)*(x) <= 0.0))
 # else
-#  define DOUBLE_ISNAN(x) (&(x) == &(x) && (x) != (x))
+#  define DOUBLE_ISNAN(x) (LVALUE(x) && (x) != (x))
 # endif
 #endif
 